6ff9100900acf6b67f05006854a9f45f6217aae8
mantrupe <mmaannuueell95@hotmail.com>
2017-05-27 15:35:25 -0500

    Options
    updtate

--- Controlador/index.js
+++ Controlador/index.js
@@ -1,18 +1,28 @@
-var texto = $("#txt");
-var btn = $("#enviar");
+$(function() {
+    var texto = $("#txt");
+    var btn = $("#enviar");
 
-btn.click(function(){
-  var parameters = texto.val();
-  $.ajax({
-      method: "GET",
-      url: "http://localhost:4000/55030a6884a8aee342aff85d70a418c5/v1",
-      data: parameters,
-      dataType:"json",
-      error: function(){
-        console.log("error","Error en el servidor");  //Error de servidor
-      },
-      success: function(data){
-        console.log(data);
-      }
+    $('#Controlador').spinner({
+               min: 2,
+               max: 20,
+               step: 2
+    });
 
+    btn.click(function(){
+      var parameters = texto.val();
+      $.ajax({
+          method: "GET",
+          url: "http://localhost:4000/55030a6884a8aee342aff85d70a418c5/v1",
+          data: parameters,
+          dataType:"json",
+          error: function(){
+            console.log("error","Error en el servidor");  //Error de servidor
+          },
+          success: function(data){
+            console.log(data);
+          }
+
+    });
+
+  });
 });
